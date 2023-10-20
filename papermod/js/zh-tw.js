08e7b47bec6be420ad16603b22d33f177ae7a3a3
diy99 <fluxbbtw@gmail>
2023-10-21 06:03:24 +0800

    Update

---
+++ papermod/js/zh-tw.js
@@ -0,0 +1,20 @@
+window.CUSDIS_LOCALE = {
+    powered_by: 'Comments powered by Cusdis',
+	post_comment: '發送留言 - Comment',
+    loading: '加載中 - Loading',
+  
+    // comment
+    email: '信箱(選填) - Email (optional)',
+    nickname: ' 暱稱 - Nickname',
+    reply_placeholder: '留言內容 - Reply...',
+    reply_btn: '回覆 - Reply',
+    sending: '發送中… - sending...',
+  
+    // reply
+    mod_badge: '管理員(MOD)',
+    content_is_required: '內容不能為空白 - Content is required',
+    nickname_is_required: '暱稱不能為空白 - Nickname is required',
+  
+    comment_has_been_sent:
+      '評論已發送，請等待審核 - Your comment has been sent. Please wait for approval.',
+  }
